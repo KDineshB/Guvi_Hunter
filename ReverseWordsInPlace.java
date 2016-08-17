b9a75f7b7ca8a84d00dd5705a724b7566548b79c
K Dinesh Babu <dineshbabuk14@gmail.com>
2016-08-18 01:05:27 +0530

    Add files via upload

---
+++ ReverseWordsInPlace.java
@@ -0,0 +1,19 @@
+package hunter;
+
+import java.util.Scanner;
+
+public class ReverseWordsInPlace {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String sentence = scanner.nextLine();
+		String[] words = sentence.split(" ");
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < words.length; i++) {
+			buffer.append(new StringBuffer(words[i]).reverse().toString()+" ");
+		}
+		System.out.println(buffer);
+		scanner.close();
+	}
+
+}
